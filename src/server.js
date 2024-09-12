8bab58971d6abab0b5d94caf8a5808f830a165df
Dương Trí Dũng <crytv778@gmail.com>
2024-09-12 13:42:16 +0700

    62. CORS đâu có lỗi lầm gì?

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-console */
 import express from "express";
-import { CLOSE_DB, CONNECT_DB } from "./config/mongodb";
 import exitHook from "async-exit-hook";
+import cors from "cors";
+import { CLOSE_DB, CONNECT_DB } from "./config/mongodb";
 import { env } from "./config/environment";
 import { APIs_V1 } from "./routes/v1";
 import { errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware";
+import { corsOptions } from "./config/cors";
 
 const START_SERVER = () => {
   const app = express();
+
+  app.use(cors(corsOptions));
 
   const hostname = env.APP_HOST;
   const port = env.APP_PORT;
