eb25ea5335be7333dab1546ad3d99da772090183
Dương Trí Dũng <crytv778@gmail.com>
2024-09-12 10:26:47 +0700

    54. Error Handling: Middleware xử lý lỗi tập trung phía Back-end

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,12 +1,15 @@
 import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError";
 
 const createNew = async (req, res, next) => {
   try {
-    res.status(StatusCodes.CREATED).json({ mes: "Controller: create a board" });
+    // res.status(StatusCodes.CREATED).json({ mes: "Controller: create a board" });
+    throw new ApiError(StatusCodes.BAD_GATEWAY, "Test error");
   } catch (error) {
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ errors: error.message });
+    next(error);
+    // res
+    //   .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    //   .json({ errors: error.message });
   }
 };
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { CLOSE_DB, CONNECT_DB } from "./config/mongodb";
 import exitHook from "async-exit-hook";
 import { env } from "./config/environment";
 import { APIs_V1 } from "./routes/v1";
+import { errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware";
 
 const START_SERVER = () => {
   const app = express();
@@ -14,6 +15,8 @@ const START_SERVER = () => {
   app.use(express.json());
 
   app.use("/v1", APIs_V1);
+
+  app.use(errorHandlingMiddleware);
 
   app.listen(port, hostname, () => {
     console.log(
