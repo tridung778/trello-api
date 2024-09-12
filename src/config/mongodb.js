74a358e8c923413aa43e48da17b6e7374f4f2da8
Dương Trí Dũng <crytv778@gmail.com>
2024-09-12 08:44:58 +0700

    49. Tổ chức biến môi trường ENV đúng cách | NodeJS + MongoDB | TrungQuanDev

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,31 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "./environment";
+const MONGODB_URL = env.MONGODB_URI;
+
+const DATABASE_NAME = env.DATABASE_NAME;
+
+let trelloDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(MONGODB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+};
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error("Must connect database first!");
+
+  return trelloDatabaseInstance;
+};
--- src/server.js
+++ src/server.js
@@ -1,33 +1,34 @@
-/* eslint-disable array-bracket-spacing */
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-console */
+import express from "express";
+import { CLOSE_DB, CONNECT_DB, GET_DB } from "./config/mongodb";
+import exitHook from "async-exit-hook";
+import { env } from "./config/environment";
 
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+const START_SERVER = () => {
+  const app = express();
 
-const app = express()
+  const hostname = env.APP_HOST;
+  const port = env.APP_PORT;
 
-const hostname = 'localhost'
-const port = 8017
+  app.get("/", async (req, res) => {
+    const listColec = await GET_DB().listCollections().toArray();
+    res.end(JSON.stringify(listColec));
+  });
 
-app.get('/', (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(port, hostname, () => {
+    console.log(
+      `Hello ${env.AUTHOR}, I am running at http://${hostname}:${port}/`
+    );
+  });
+  exitHook(() => {
+    CLOSE_DB();
+  });
+};
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Tri Dung, I am running at ${ hostname }:${ port }/`)
-})
+CONNECT_DB()
+  .then(() => console.log("connected MongoDB"))
+  .then(() => START_SERVER())
+  .catch((error) => {
+    console.error(error);
+    process.exit(0);
+  });
